266163358fce78cd31253a4805213858aa9fbafb
Eric Lybrand <elybrand@ucsd.edu>
2020-04-05 18:25:54 -0700

    Plot neuron dashboard.

--- quant_net_exploratory.py
+++ quant_net_exploratory.py
@@ -3,9 +3,10 @@ from scipy.linalg import norm
 from keras.models import Sequential
 from keras.layers import Dense
 from matplotlib import pyplot as plt
-%matplotlib osx
+# %matplotlib osx
+from quantized_network import QuantizedNeuralNetwork
 
-if __name__ = "__main__":
+if __name__ == "__main__":
 
 	N0 = 10
 	N1 = 32
@@ -24,4 +25,7 @@ if __name__ = "__main__":
 
 	my_quant_net = QuantizedNeuralNetwork(model, batch_size, get_batch_data)
 	my_quant_net.quantize_network()
+
+	neuron_axes = my_quant_net.neuron_dashboard(0,0)
+	plt.show()
 
