d5259486479a5709cabd7f24774067740a163567
Eric Lybrand <elybrand@ucsd.edu>
2020-06-12 11:04:16 -0700

    Remove all multithreading. Remove residual tensors. Fix bug that did not scale alphabet appropriately for QCNN class.

--- scripts/quantized_network.py
+++ scripts/quantized_network.py
@@ -19,7 +19,6 @@ from collections import namedtuple
 from itertools import product
 from matplotlib.pyplot import subplots
 from matplotlib.axes import Axes
-from concurrent.futures import ThreadPoolExecutor, as_completed
 
 QuantizedNeuron = namedtuple("QuantizedNeuron", ["layer_idx", "neuron_idx", "q", "U"])
 QuantizedFilter = namedtuple(
@@ -35,7 +34,6 @@ class QuantizedNeuralNetwork:
         batch_size: int,
         get_data: Generator[array, None, None],
         logger=None,
-        is_debug=False,
         ignore_layers=[],
         order=1,
         bits=log2(3),
@@ -45,7 +43,7 @@ class QuantizedNeuralNetwork:
         CAVEAT: Bias terms are not quantized!
         REMEMBER: TensorFlow flips everything for you. Networks act via
 
-        # TODO: add verbose flag
+        # TODO: add verbose flag, progress bar
 
         batch_size x N_ell     N_ell x N_{ell+1}
 
@@ -98,54 +96,9 @@ class QuantizedNeuralNetwork:
         # distribution of that layer's weights.
         self.alphabet = linspace(-1, 1, num=int(round(2 ** (bits))))
 
-        # A dictionary with key being the layer index ell and the values being tensors.
-        # self.residuals[ell][neuron_idx, :] is a N_ell x batch_size matrix storing the residuals
-        # generated while quantizing the neuron_idx neuron.
-        self.residuals = {
-            layer_idx: zeros(
-                (
-                    dims[1],  # N_{ell+1} neurons,
-                    int(
-                        dims[0] / order
-                    ),  # N_{ell} dimensional feature, order bits per iteration.
-                    self.batch_size,
-                )
-            )  # Dimension of the residual vectors.
-            for layer_idx, dims in self.layer_dims.items()
-        }
-
-        # Logs the relative error between the data fed through the unquantized network and the
-        # quantized network.
-        self.layerwise_rel_errs = {
-            layer_idx: zeros(dims[0],)  # N_{ell} dimensional feature,
-            for layer_idx, dims in self.layer_dims.items()
-        }
-
         self.logger = logger
 
         self.ignore_layers = ignore_layers
-
-        self.is_debug = is_debug
-
-        # This is used to log the directions which are used to choose the bits.
-        if self.is_debug:
-            self.layerwise_directions = {
-                layer_idx: {
-                    "wX": zeros(
-                        (
-                            self.batch_size,  # B vectors in each batch
-                            dims[0],
-                        )  # N_ell dimensional feature
-                    ),
-                    "qX": zeros(
-                        (
-                            self.batch_size,  # B vectors in each batch
-                            dims[0],
-                        )  # N_ell dimensional feature
-                    ),
-                }
-                for layer_idx, dims in self.layer_dims.items()
-            }
 
     def bit_round(self, t: float, rad: float) -> int:
 
@@ -353,53 +306,16 @@ class QuantizedNeuralNetwork:
         # Set the radius of the alphabet.
         rad = self.alphabet_scalar * median(abs(W.flatten()))
 
-        # Now quantize the neurons.
-        with ThreadPoolExecutor() as executor:
-
+        for neuron_idx in range(N_ell_plus_1):
             if order == 1:
-                future_to_neuron = {
-                    executor.submit(
-                        self.quantize_neuron, layer_idx, neuron_idx, wX, qX, rad
-                    ): neuron_idx
-                    for neuron_idx in range(N_ell_plus_1)
-                }
+                qNeuron = self.quantize_neuron(layer_idx, neuron_idx, wX, qX, rad)
             else:
-                future_to_neuron = {
-                    executor.submit(
-                        self.quantize_neuron2, layer_idx, neuron_idx, wX, qX, rad
-                    ): neuron_idx
-                    for neuron_idx in range(N_ell_plus_1)
-                }
-            for future in as_completed(future_to_neuron):
-                neuron_idx = future_to_neuron[future]
-                try:
-                    qNeuron = future.result()
-                except Exception as exc:
-                    if self.logger:
-                        self.logger.error(
-                            f"Error quantizing neuron {neuron_idx} in layer {layer_idx}: {exc}"
-                        )
-                    else:
-                        print(
-                            f"Error quantizing neuron {neuron_idx} in layer {layer_idx}: {exc}"
-                        )
-                # Update quantized weight matrix and the residual dictionary.
-                Q[:, neuron_idx] = qNeuron.q
-                self.residuals[layer_idx][neuron_idx, :] = qNeuron.U
-                if self.logger:
-                    self.logger.info(
-                        f"\tFinished quantizing neuron {neuron_idx} of {N_ell_plus_1}"
-                    )
-
-        # for neuron_idx in range(N_ell_plus_1):
-        #   if order == 1:
-        #       qNeuron = self.quantize_neuron(layer_idx, neuron_idx, wX, qX, rad)
-        #   else:
-        #       qNeuron = self.quantize_neuron2(layer_idx, neuron_idx, wX, qX, rad)
-        #   Q[:, neuron_idx] = qNeuron.q
-        #   self.residuals[layer_idx][neuron_idx,:] = qNeuron.U
-        #   if self.logger:
-        #       self.logger.info(f"\tFinished quantizing neuron {neuron_idx} of {N_ell_plus_1}")
+                qNeuron = self.quantize_neuron2(layer_idx, neuron_idx, wX, qX, rad)
+            Q[:, neuron_idx] = qNeuron.q
+            if self.logger:
+                self.logger.info(
+                    f"\tFinished quantizing neuron {neuron_idx} of {N_ell_plus_1}"
+                )
 
         # Update the quantized network. Use the same bias vector as in the analog network for now.
         if self.trained_net.layers[layer_idx].use_bias:
@@ -439,83 +355,9 @@ class QuantizedNeuralNetwork:
                     self.logger.info(f"Quantizing layer {layer_idx}...")
                 self.quantize_layer(layer_idx, self.order)
 
-    def neuron_dashboard(self, layer_idx: int, neuron_idx: int) -> Axes:
-
-        w = self.trained_net.get_weights()[layer_idx][:, neuron_idx]
-        q = self.quantized_net.get_weights()[layer_idx][:, neuron_idx]
-        N_ell = w.shape[0]
-
-        fig, axes = subplots(2, 2, figsize=(10, 10))
-
-        fig.suptitle(
-            f"Dashboard for Neuron {neuron_idx} in Layer {layer_idx}", fontsize=24
-        )
-
-        resid_ax = axes[0, 0]
-        resid_ax.set_title("Norm of Residuals")
-        resid_ax.set_xlabel(r"t", fontsize=14)
-        resid_ax.set_ylabel(r"$||u_t||$", fontsize=14)
-        resid_ax.plot(
-            range(N_ell),
-            [norm(self.residuals[layer_idx][neuron_idx, t, :]) for t in range(N_ell)],
-            "-o",
-        )
-
-        # TODO: What could go in axes[0,1]?
-
-        w_ax = axes[1, 0]
-        w_ax.set_title("Histogrammed Weights")
-        w_ax.hist(w)
-
-        q_ax = axes[1, 1]
-        q_ax.set_title("Histogrammed Bits")
-        q_ax.hist(q)
-
-        return axes
-
-    def layer_dashboard(self, layer_idx: int) -> Axes:
-
-        W = self.trained_net.layers[layer_idx].get_weights()[0]
-        Q = self.quantized_net.layers[layer_idx].get_weights()[0]
-        U_tensor = self.residuals[layer_idx]
-        N_ell, N_ell_plus_1 = W.shape
-
-        fig, axes = subplots(2, 2, figsize=(10, 10))
-        fig.suptitle(
-            f"Dashboard for Layer {layer_idx}: $(N_{{L}}, N_{{L+1}}, d)$ = ({N_ell}, {N_ell_plus_1}, {self.batch_size})"
-        )
-        # adjust the spacing between axes
-        fig.tight_layout(pad=6.0)
-
-        # For every t, plot sup_{neurons} ||u_t||
-        sup_resids = [
-            max([norm(U_tensor[neuron_idx, t, :]) for neuron_idx in range(N_ell_plus_1)])
-            for t in range(N_ell)
-        ]
-        resid_ax = axes[0, 0]
-        resid_ax.set_title("Supremal Residual Across Neurons")
-        resid_ax.hist(sup_resids)
-
-        # Plot relative error of data pushed through the quantized net.
-        rel_errs_ax = axes[0, 1]
-        rel_errs_ax.set_title(r"Histogram of $\frac{||\Delta X_t||}{||X_t||}$")
-        rel_errs_ax.hist(self.layerwise_rel_errs[layer_idx])
-
-        # Histogram the population of weights as well as the bits.
-        w_hist = axes[1, 0]
-        w_hist.set_title("Histogram of Weights")
-        w_hist.hist(W.flatten())
-
-        q_hist = axes[1, 1]
-        q_hist.set_title("Histogram of Bits")
-        q_hist.hist(Q.flatten())
-
-        return axes
-
 
 class QuantizedCNN(QuantizedNeuralNetwork):
 
-    # TODO: Add logging option
     # TODO: Add ignore_layer option
 
     def __init__(
@@ -524,7 +366,6 @@ class QuantizedCNN(QuantizedNeuralNetwork):
         batch_size: int,
         get_data: Generator[array, None, None],
         logger=None,
-        is_debug=False,
         bits=log2(3),
         alphabet_scalar=1,
     ):
@@ -708,34 +549,42 @@ class QuantizedCNN(QuantizedNeuralNetwork):
         # Set the radius of the alphabet.
         rad = self.alphabet_scalar * median(abs(W.flatten()))
 
-        # Now quantize the neurons.
-        with ThreadPoolExecutor() as executor:
-            future_to_neuron = {
-                executor.submit(
-                    self.quantize_neuron, layer_idx, neuron_idx, wX, qX, rad
-                ): neuron_idx
-                for neuron_idx in range(N_ell_plus_1)
-            }
-            for future in as_completed(future_to_neuron):
-                neuron_idx = future_to_neuron[future]
-                try:
-                    qNeuron = future.result()
-                except Exception as exc:
-                    if self.logger:
-                        self.logger.error(
-                            f"Error quantizing neuron {neuron_idx} in layer {layer_idx}: {exc}"
-                        )
-                    else:
-                        print(
-                            f"Error quantizing neuron {neuron_idx} in layer {layer_idx}: {exc}"
-                        )
-                # Update quantized weight matrix.
-                Q[:, neuron_idx] = qNeuron.q
-                if self.logger:
-                    self.logger.info(
-                        f"\tFinished quantizing neuron {neuron_idx} of {N_ell_plus_1}"
-                    )
+        # # Now quantize the neurons.
+        # with ThreadPoolExecutor() as executor:
+        #     future_to_neuron = {
+        #         executor.submit(
+        #             self.quantize_neuron, layer_idx, neuron_idx, wX, qX, rad
+        #         ): neuron_idx
+        #         for neuron_idx in range(N_ell_plus_1)
+        #     }
+        #     for future in as_completed(future_to_neuron):
+        #         neuron_idx = future_to_neuron[future]
+        #         try:
+        #             qNeuron = future.result()
+        #         except Exception as exc:
+        #             if self.logger:
+        #                 self.logger.error(
+        #                     f"Error quantizing neuron {neuron_idx} in layer {layer_idx}: {exc}"
+        #                 )
+        #             else:
+        #                 print(
+        #                     f"Error quantizing neuron {neuron_idx} in layer {layer_idx}: {exc}"
+        #                 )
+        #         # Update quantized weight matrix.
+        #         Q[:, neuron_idx] = qNeuron.q
+        #         if self.logger:
+        #             self.logger.info(
+        #                 f"\tFinished quantizing neuron {neuron_idx} of {N_ell_plus_1}"
+        #             )
 
+        for neuron_idx in range(N_ell_plus_1):
+            qNeuron = self.quantize_neuron(layer_idx, neuron_idx, wX, qX, rad)
+            # Update quantized weight matrix.
+            Q[:, neuron_idx] = qNeuron.q
+            if self.logger:
+                self.logger.info(
+                    f"\tFinished quantizing neuron {neuron_idx} of {N_ell_plus_1}"
+                )
         # Update the quantized network. Use the same bias vector as in the analog network for now.
         if self.trained_net.layers[layer_idx].use_bias:
             bias = self.trained_net.layers[layer_idx].get_weights()[1]
@@ -779,50 +628,66 @@ class QuantizedCNN(QuantizedNeuralNetwork):
             wX = prev_trained_output([batch])[0]
             qX = prev_quant_output([batch])[0]
 
-        # Set the radius of the ternary alphabet.
-        alpha = 1  # This is somewhat arbitrary.
-        rad = alpha * median(abs(W.flatten()))
+        # Set the radius of the alphabet.
+        rad = self.alphabet_scalar * median(abs(W.flatten()))
 
         Q = zeros(W.shape)
 
-        # Now quantize the neurons.
-        with ThreadPoolExecutor() as executor:
-            future_to_filter = {
-                executor.submit(
-                    self.quantize_filter2D, layer_idx, filter_idx, wX, qX, rad
-                ): filter_idx
-                for filter_idx in range(num_filters)
-            }
-            for future in as_completed(future_to_filter):
-                filter_idx = future_to_filter[future]
-                try:
-                    quantized_chan_filter_list = future.result()
-                except Exception as exc:
-                    if self.logger:
-                        self.logger.error(
-                            f"Error quantizing filter {filter_idx} in layer {layer_idx}: {exc}"
-                        )
-                    else:
-                        print(
-                            f"Error quantizing neuron {filter_idx} in layer {layer_idx}: {exc}"
-                        )
+        # # Now quantize the neurons.
+        # with ThreadPoolExecutor() as executor:
+        #     future_to_filter = {
+        #         executor.submit(
+        #             self.quantize_filter2D, layer_idx, filter_idx, wX, qX, rad
+        #         ): filter_idx
+        #         for filter_idx in range(num_filters)
+        #     }
+        #     for future in as_completed(future_to_filter):
+        #         filter_idx = future_to_filter[future]
+        #         try:
+        #             quantized_chan_filter_list = future.result()
+        #         except Exception as exc:
+        #             if self.logger:
+        #                 self.logger.error(
+        #                     f"Error quantizing filter {filter_idx} in layer {layer_idx}: {exc}"
+        #                 )
+        #             else:
+        #                 print(
+        #                     f"Error quantizing neuron {filter_idx} in layer {layer_idx}: {exc}"
+        #                 )
 
-                # Now we need to stack all the channel information together again.
-                N, B = quantized_chan_filter_list[0].U.shape
-                filter_U = zeros((N, B, num_channels))
-                # Again, following Tensorflow convention that the channel information is the l ast component.
-                quantized_filter = zeros((filter_shape[0], filter_shape[1], num_channels))
-                for channel_filter in quantized_chan_filter_list:
-                    channel_idx = channel_filter.channel_idx
-                    filter_U[:, :, channel_idx] = channel_filter.U
-                    quantized_filter[:, :, channel_idx] = channel_filter.q_filtr
+        #         # Now we need to stack all the channel information together again.
+        #         N, B = quantized_chan_filter_list[0].U.shape
+        #         filter_U = zeros((N, B, num_channels))
+        #         # Again, following Tensorflow convention that the channel information is the l ast component.
+        #         quantized_filter = zeros((filter_shape[0], filter_shape[1], num_channels))
+        #         for channel_filter in quantized_chan_filter_list:
+        #             channel_idx = channel_filter.channel_idx
+        #             quantized_filter[:, :, channel_idx] = channel_filter.q_filtr
 
-                Q[:, :, :, filter_idx] = quantized_filter
+        #         Q[:, :, :, filter_idx] = quantized_filter
 
-                if self.logger:
-                    self.logger.info(
-                        f"\tFinished quantizing filter {filter_idx} of {num_filters}"
-                    )
+        #         if self.logger:
+        #             self.logger.info(
+        #                 f"\tFinished quantizing filter {filter_idx} of {num_filters}"
+        #             )
+
+        for filter_idx in range(num_filters):
+            quantized_chan_filter_list = self.quantize_filter2D(
+                layer_idx, filter_idx, wX, qX, rad
+            )
+            # Now we need to stack all the channel information together again.
+            # Again, following Tensorflow convention that the channel information is the last component.
+            quantized_filter = zeros((filter_shape[0], filter_shape[1], num_channels))
+            for channel_filter in quantized_chan_filter_list:
+                channel_idx = channel_filter.channel_idx
+                quantized_filter[:, :, channel_idx] = channel_filter.q_filtr
+
+            Q[:, :, :, filter_idx] = quantized_filter
+
+            if self.logger:
+                self.logger.info(
+                    f"\tFinished quantizing filter {filter_idx} of {num_filters}"
+                )
 
         # Now update the layer in the quantized network. Leave the bias the same for now.
         if self.trained_net.layers[layer_idx].use_bias:
