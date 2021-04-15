051e3f7550d8d8d17205a35fd9d76135a6bfcf5a
elybrand <lybrand.eric@gmail.com>
2021-04-15 11:31:23 -0700

    Refactor preprocess imagenet script so that it just dumps preprocessed images into one directory. This pushes train test split to the quantization script and simplifies the preprocessing script. Multiprocess preprocessing script.

--- scripts/preprocess_imagenet.py
+++ scripts/preprocess_imagenet.py
@@ -10,6 +10,7 @@ import numpy as np
 import scipy.io
 import cv2
 import shutil
+import concurrent.futures
 from time import time
 
 # The code in this script is generously provided by Caleb Robinson from his github repo which
@@ -17,81 +18,28 @@ from time import time
 # to that repo on Caleb's website.
 # https://calebrob.com/ml/imagenet/ilsvrc2012/2018/10/22/imagenet-benchmarking.html.
 #
-# This script shards and resizes images to 224x224. Importantly, it does not preprocess the images
-# according to pretrained model specifications. This is because the pretrained models all
-# preprocess in different manners, so that is left to the script which evaluates the networks on
-# the test data.
+# This script applies baseline preprocessing for all pretrained models, and resizes images to 224x224. 
+# Importantly, however, it does not further preprocess the images according to pretrained model specifications. 
+# This is because the pretrained models all preprocess in different manners, so that is left to the script 
+# which evaluates the networks on the test data.
 
-def humansize(nbytes):
-    '''From https://stackoverflow.com/questions/14996453/python-libraries-to-calculate-human-readable-filesize-from-bytes'''
-    suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
-    i = 0
-    while nbytes >= 1024 and i < len(suffixes)-1:
-        nbytes /= 1024.
-        i += 1
-    f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
-    return '%s %s' % (f, suffixes[i])
+dir_imagenet_val_dataset = Path("../data/") # path/to/data/
+dir_images = Path("../data/val") # path/to/images/directory
+dir_processed_images = Path("../data/preprocessed_val/")
+path_labels = Path("../data/ILSVRC2012_validation_ground_truth.txt")
+path_synset_words = Path("../data/synset_words.txt")
+path_meta = Path("../data/meta.mat")
 
-def load_images(image_paths, returned_shard, n_shards=5):
-    """ loads images into memory. It only load and returns images of the 'returned_shard'.
-        image_paths: a list of paths to images
-        n_shards: number of shards to loaded images be divided.
-        returned_shard: the part of images to be returned. 0 <= returned_shard < n_shards
-    """
-    assert 0 <= returned_shard < n_shards, "The argument returned_shard must be between 0 and n_shards"
-    
-    shard_size = len(image_paths) // n_shards
-    sharded_image_paths = image_paths[returned_shard*shard_size:(returned_shard+1)*shard_size] if returned_shard < n_shards - 1 \
-                     else image_paths[returned_shard*shard_size:]
-    
-    images_list = np.zeros((len(sharded_image_paths), 224, 224, 3), dtype=np.uint8)
-    
-    for i, image_path in enumerate(sharded_image_paths):
-        
-        # Load (in BGR channel order)
-        image = cv2.imread(image_path)
-        
-        
-        # Resize
-        height, width, _ = image.shape
-        new_height = height * 256 // min(image.shape[:2])
-        new_width = width * 256 // min(image.shape[:2])
-        image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
-
-        # Crop
-        height, width, _ = image.shape
-        startx = width//2 - (224//2)
-        starty = height//2 - (224//2)
-        image = image[starty:starty+224,startx:startx+224]
-        assert image.shape[0] == 224 and image.shape[1] == 224, (image.shape, height, width)
-        
-
-        images_list[i, ...] = image[..., ::-1]
-        
-    return images_list
-
-def shard_images(n_shards=500):
-
-    for i in range(n_shards):
-        
-        images = load_images(image_paths, returned_shard=i, n_shards=n_shards)
-        
-        if i == 0:
-            print("Total memory allocated for loading images:", humansize(images.nbytes))
-        
-        np.save(str(path_imagenet_val_dataset / "x_val_{}.npy".format(i+1)), images)
-        
-        if (i + 1) * 100 / n_shards % 5 == 0:
-            print("{:.0f}% Completed.".format((i+1)/n_shards*100))
-        
-        images = None
+def _clean_up_processed_images():
+    # Migrate nested training image directory structure back to unstructured validation directory.
+    if dir_processed_images.exists():
+        shutil.rmtree(dir_processed_images)
 
 def _preprocess_image(image_path):
 
         # Load (in BGR channel order)
         image = cv2.imread(image_path)
         
-        
         # Resize
         height, width, _ = image.shape
         new_height = height * 256 // min(image.shape[:2])
@@ -106,7 +54,13 @@ def _preprocess_image(image_path):
         assert image.shape[0] == 224 and image.shape[1] == 224, (image.shape, height, width)
         
 
-        return image[..., ::-1]
+        # Grab the name of the image file without the file extension.
+        filename = image_path.split('/')[-1].split('.')[0]
+        new_filename = "preprocessed_" + filename + ".npy"
+        new_image_path = str(dir_processed_images) + "/" + new_filename
+
+        # Save the image to disk, making sure to reorder channels into RGB order.
+        np.save(new_image_path, image[..., ::-1])
 
 def generate_labels():
     for i in range(1000):
@@ -130,49 +84,7 @@ def generate_labels():
         y_val = f.read().strip().split("\n")
         y_val = np.array([convert_original_idx_to_keras_idx(int(idx)) for idx in y_val])
 
-    np.save(str(path_imagenet_val_dataset/"y_val.npy"), y_val)
-    return y_val
-
-def _clean_up_train_test_split():
-    # Migrate nested training image directory structure back to unstructured validation directory.
-    if path_train_images.exists():
-        print("Existing training image population exists. Cleaning up...", end="")
-        nested_image_paths = glob(str(path_train_images/"*/*.npy"))
-        for image_path in nested_image_paths:
-            os.remove(image_path)
-        # Get rid of the now empty training directory.
-        for nested_dir in os.listdir(path_train_images):
-            os.rmdir(f"{path_train_images}/{nested_dir}")
-        os.rmdir(path_train_images)
-        print("done.")
-
-    # Migrate nested test image directory structure back to unstructured validation directory.
-    if path_test_images.exists():
-        print("Existing test image population exists. Cleaning up...", end="")
-        nested_image_paths = glob(str(path_test_images/"*/*.npy"))
-        for image_path in nested_image_paths:
-            os.remove(image_path)
-        # Get rid of the now empty test directory
-        for nested_dir in os.listdir(path_test_images):
-            os.rmdir(f"{path_test_images}/{nested_dir}")
-        os.rmdir(path_test_images)
-        print("done.")
-
-def _add_image_to_dir(image_path, label, parent_dir):
-
-    # Grab the filename of this image without the path or the file extension.
-    image_filename =  image_path.split('/')[-1].split('.')[-2]
-    # Initialize what the nested subdirectory's name is.
-    image_directory = str(parent_dir) + f"/{label}"
-    # Determine if you need to create a nested subdirectory for this new image.
-    if str(label) not in os.listdir(parent_dir):
-        os.mkdir(image_directory)
-
-    # Preprocess image, then save preprocessed image into labelled subdirectory.
-    image = _preprocess_image(image_path)
-    new_imagepath_name = str(image_directory) + "/preprocessed_" + str(image_filename)
-    np.save(new_imagepath_name, image)
-    # shutil.copy(image_path, image_directory)
+    np.save(str(dir_imagenet_val_dataset/"y_val.npy"), y_val)
 
 def train_test_split(labels):
 
@@ -203,47 +115,25 @@ def train_test_split(labels):
 
 if __name__ == "__main__":
 
-    path_imagenet_val_dataset = Path("../data/") # path/to/data/
-    path_train_images = Path("../train_data/")
-    path_test_images = Path("../test_data/")
-    dir_images = Path("../data/val") # path/to/images/directory
-    path_labels = Path("../data/ILSVRC2012_validation_ground_truth.txt")
-    path_synset_words = Path("../data/synset_words.txt")
-    path_meta = Path("../data/meta.mat")
-
-    # If an existing split is present, then delete it.
-    _clean_up_train_test_split()
-
     image_paths = sorted(glob(str(dir_images/"*")))
     n_images = len(image_paths)
-
-    # Grab the command line input for number of images to use for quantization training.
-    quantization_training_size = int(sys.argv[1])
-    test_size = n_images - quantization_training_size
-    if test_size < 0:
-        raise ValueError(f"{quantization_training_size} images were requested to be used for learning quantization, but there are only {n_images} images in total.")
-
     meta = scipy.io.loadmat(str(path_meta))
     original_idx_to_synset = {}
     synset_to_name = {}
 
+    _clean_up_processed_images()
 
-    # Shard the images for quicker I/O.
-    # shard_images()
+    os.mkdir(dir_processed_images)
 
     # Use the devkit to generate labels for validation data
-    labels = generate_labels()
-
-    try:
-        tic = time()
-        print(f"Preprocessing and splitting training and test populations...")
-        train_test_split(labels)
-        print(f"done preprocessing and splitting. {time() - tic:.2f} seconds.")
-    except Exception as exc:
-        print(f"An error occured in splitting. Cleaning up partial split.")
-        _clean_up_train_test_split()
-        print("done with clean up.")
-        raise(exc)
+    generate_labels()
+    print("Preprocessing data...")
+    tic = time()
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        future_to_path = {executor.submit(_preprocess_image, image_path): image_path for image_path in image_paths}
+        for future in concurrent.futures.as_completed(future_to_path):
+                future.result()
+    print(f"done. {time() - tic:.2f} seconds.")
 
 
 
