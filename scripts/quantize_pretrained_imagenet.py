6265582d7ec3af30a160cc02099bb0929b8a4ec7
elybrand <lybrand.eric@gmail.com>
2021-05-13 11:12:29 -0700

    deprecate image_generator. Update logging. Import vgg16.

--- scripts/quantize_pretrained_imagenet.py
+++ scripts/quantize_pretrained_imagenet.py
@@ -7,11 +7,12 @@ from tensorflow import argsort, cast, transpose, argmax, int32, float32
 from tensorflow.math import reduce_any, reduce_mean
 from tensorflow.random import set_seed
 from tensorflow.keras.models import load_model, clone_model, save_model
-from tensorflow.keras.applications import ResNet50, MobileNet
+from tensorflow.keras.applications import VGG16, ResNet50, MobileNet
+from tensorflow.keras.applications.vgg16 import preprocess_input as vgg16_preprocess_input
 from tensorflow.keras.applications.resnet import preprocess_input as resnet_preprocess_input
 from tensorflow.keras.applications.mobilenet import preprocess_input as mobilenet_preprocess_input
 from tensorflow.keras.utils import to_categorical
-from quantized_network import QuantizedCNN, ImageNetSequence
+from quantized_network import QuantizedCNN, ImageNetSequence, _bit_round_parallel
 from itertools import chain
 from time import time
 from sys import stdout, argv
@@ -38,12 +39,13 @@ dir_imagenet_val_dataset = Path("../data/")
 dir_processed_images = Path("../data/preprocessed_val/")
 
 # Grab the pretrained model name
-pretrained_model = [MobileNet]
-preprocess_func = [mobilenet_preprocess_input]
+
+pretrained_model = [VGG16]
+preprocess_func = [vgg16_preprocess_input]
 data_sets = ["ILSVRC2012"]
-q_train_sizes = [25000]
-bits = [np.log2(i) for i in  (16,)]
-alphabet_scalars = [2, 3, 4, 5, 6]
+q_train_sizes = [5000]
+bits = [4, 5, 6, 7, 8]
+alphabet_scalars = list(range(1, 20))
 
 np_seed = 0
 tf_seed = 0
@@ -66,16 +68,9 @@ param_iterable = (ParamConfig(*config) for config in parameter_grid)
 def _preprocess_and_reshape(image_path, preprocess_func):
     image = np.load(image_path)
     preprocessed_image = preprocess_func(image)
+    # TODO: reshaping into 4 tensor is bad.
     reshaped_image = np.reshape(preprocessed_image, (1, *preprocessed_image.shape))
     return reshaped_image
-
-def get_image_generator(image_paths, preprocess_func, epochs=1):
-    image_iterator = (_preprocess_and_reshape(image_path, preprocess_func) for image_path in image_paths)
-    for epoch in range(1, epochs):
-        # Chain together another copy of base_iterator if epochs > 1.
-        image_iterator = chain(image_iterator, (_preprocess_and_reshape(image_path, preprocess_func) for image_path in image_paths))
-
-    return image_iterator
 
 def top_k_accuracy(y_true, y_pred, k=1, tf_enabled=True):
     '''
@@ -92,6 +87,22 @@ def top_k_accuracy(y_true, y_pred, k=1, tf_enabled=True):
         return np.any(argsorted_y.T == y_true.argmax(axis=1), axis=0).mean()
 
 def quantize_network(parameters: ParamConfig) -> pd.DataFrame:
+
+    # Load the model with imagenet weights and the top layer included.
+    model = parameters.pretrained_model()
+    model.compile(
+        optimizer="sgd", loss="categorical_crossentropy", metrics=["accuracy"]
+    )
+    logger.info(f"""Quantizing with the following parameters:
+            data_set: {parameters.data_set},
+            serialized_model: {model.name},
+            q_train_size: {parameters.q_train_size},
+            bits: {parameters.bits},
+            alphabet_scalar: {parameters.alphabet_scalar},
+            np_seed: {np_seed},
+            tf_seed: {tf_seed}
+        """
+        )
 
     # Reset the seeds for splitting training and testing
     np.random.seed(np_seed)
@@ -116,12 +127,6 @@ def quantize_network(parameters: ParamConfig) -> pd.DataFrame:
     y_train = to_categorical(y_train, num_classes)
     y_test = to_categorical(y_test, num_classes)
 
-    # Load the model with imagenet weights and the top layer included.
-    model = parameters.pretrained_model()
-    model.compile(
-        optimizer="sgd", loss="categorical_crossentropy", metrics=["accuracy"]
-    )
-
     logger.info("Generating analog predicted labels...")
     tic = time()
     test_generator = ImageNetSequence(test_paths, y_test, batch_size=32, preprocess_func=parameters.preprocess_func)
@@ -130,19 +135,18 @@ def quantize_network(parameters: ParamConfig) -> pd.DataFrame:
     top1_analog = top_k_accuracy(y_test, y_test_pred_analog, k=1, tf_enabled=True)
     top5_analog = top_k_accuracy(y_test, y_test_pred_analog, k=5, tf_enabled=True)
 
-    # train_generator = get_image_generator(train_paths, parameters.preprocess_func, epochs=1)
+    # train_generator = ImageNetSequence(train_paths, y_train, batch_size=32, preprocess_func=parameters.preprocess_func)
     # y_train_pred_analog = model.predict(train_generator, verbose=True)
     # top1_analog = top_k_accuracy(y_train, y_train_pred_analog, k=1, tf_enabled=True)
     # top5_analog = top_k_accuracy(y_train, y_train_pred_analog, k=5, tf_enabled=True)
 
     logger.info(f"Analog network (top 1 accuracy, top 5 accuracy) = ({top1_analog:.2f}, {top5_analog:.2f})")
 
-
     # Find out how many layers you're going to quantize. This tells us how many
     # times we need to chain together the training image generator.
     layer_names = np.array([layer.__class__.__name__ for layer in model.layers])
     num_layers_to_quantize = sum((layer_names == 'Dense') + (layer_names == 'Conv2D') + (layer_names == 'DepthwiseConv2D'))
-    quantization_train_generator = get_image_generator(train_paths, parameters.preprocess_func, epochs=num_layers_to_quantize)
+    quantization_train_generator = ImageNetSequence(train_paths, y_train, batch_size=32, preprocess_func=parameters.preprocess_func)
 
     my_quant_net = QuantizedCNN(
         network=model,
@@ -156,12 +160,15 @@ def quantize_network(parameters: ParamConfig) -> pd.DataFrame:
         tic = time()
         my_quant_net.quantize_network()
         quantization_time = time()-tic
-    except:
+    except Exception as exc:
         # Probably ran out of disk space. Clean up the patch tensors and write to logger.
         logger.info(f"An exception occurred. Cleaning up patch tensors...")
         patch_tensor_paths = sorted(glob("./*.h5"))
         for path in patch_tensor_paths:
             remove(path)
+        logger.info(exc)
+
+        raise exc
 
     my_quant_net.quantized_net.compile(
         optimizer="sgd", loss="categorical_crossentropy", metrics=["accuracy"]
@@ -192,17 +199,16 @@ def quantize_network(parameters: ParamConfig) -> pd.DataFrame:
     # Set all the weights to be equal at first. This matters for batch normalization layers.
     MSQ_model.set_weights(model.get_weights())
     for layer_idx, layer in enumerate(model.layers):
-        if layer.__class__.__name__ in ("Dense", "Conv2D"):
+        if layer.__class__.__name__ in ("Dense", "Conv2D", "DepthwiseConv2D"):
             # Use the same radius as the alphabet in the corresponding layer of the GPFQ network.
-            rad = max(
-                my_quant_net.quantized_net.layers[layer_idx].get_weights()[0].flatten()
-            )
             if MSQ_model.layers[layer_idx].use_bias:
                 W, b = model.layers[layer_idx].get_weights()
             else:
                 # There's no bias vector
                 W = model.layers[layer_idx].get_weights()[0]
-            Q = np.array([my_quant_net._bit_round(w, rad) for w in W.flatten()]).reshape(
+            rad = parameters.alphabet_scalar * np.median(np.abs(W.flatten()))
+            layer_alphabet = rad*my_quant_net.alphabet
+            Q = np.array([_bit_round_parallel(w, layer_alphabet) for w in W.flatten()]).reshape(
                 W.shape
             )
             if MSQ_model.layers[layer_idx].use_bias:
@@ -214,12 +220,12 @@ def quantize_network(parameters: ParamConfig) -> pd.DataFrame:
         optimizer="sgd", loss="categorical_crossentropy", metrics=["accuracy"]
     )
 
-    test_generator = test_generator = ImageNetSequence(test_paths, y_test, batch_size=32, preprocess_func=parameters.preprocess_func)
+    test_generator = ImageNetSequence(test_paths, y_test, batch_size=32, preprocess_func=parameters.preprocess_func)
     y_test_pred_msq = MSQ_model.predict(test_generator, verbose=True)
     top1_msq = top_k_accuracy(y_test, y_test_pred_msq, k=1, tf_enabled=True)
     top5_msq = top_k_accuracy(y_test, y_test_pred_msq, k=5, tf_enabled=True)
 
-    # train_generator = get_image_generator(train_paths, parameters.preprocess_func, epochs=1)
+    # train_generator = ImageNetSequence(train_paths, y_train, batch_size=32, preprocess_func=parameters.preprocess_func)
     # y_train_pred_msq = MSQ_model.predict(train_generator, verbose=True)
     # top1_msq = top_k_accuracy(y_train, y_train_pred_msq, k=1, tf_enabled=True)
     # top5_msq = top_k_accuracy(y_train, y_train_pred_msq, k=5, tf_enabled=True)
